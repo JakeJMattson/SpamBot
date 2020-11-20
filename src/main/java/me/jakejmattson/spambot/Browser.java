7a80457f26dbfe0766d048f6eb286ad7e5fd8449
Jake Mattson <JakeJMattson@gmail.com>
2020-11-20 01:09:33 -0600

    Code cleanup

--- src/main/java/me/jakejmattson/spambot/Browser.java
+++ src/main/java/me/jakejmattson/spambot/Browser.java
@@ -2,104 +2,91 @@ package me.jakejmattson.spambot;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.awt.*;
-import java.awt.Rectangle;
 
 /**
  * Creates and controls an individual browser.
- *
- * @author JakeJMattson
  */
-class Browser implements Runnable
-{
-	/**
-	 * Total number of active browsers
-	 */
-	private final int totalBrowsers;
-	/**
-	 * The index of this browser
-	 */
-	private final int browserIndex;
+class Browser implements Runnable {
+    /**
+     * Total number of active browsers
+     */
+    private final int totalBrowsers;
+    /**
+     * The index of this browser
+     */
+    private final int browserIndex;
 
-	Browser(int totalBrowsers, int browserIndex)
-	{
-		this.totalBrowsers = totalBrowsers;
-		this.browserIndex = browserIndex;
-	}
+    Browser(int totalBrowsers, int browserIndex) {
+        this.totalBrowsers = totalBrowsers;
+        this.browserIndex = browserIndex;
+    }
 
-	@Override
-	public void run()
-	{
-		WebDriver bot = new ChromeDriver();
-		placeBrowser(bot);
-		navigateToSpam(bot);
-		keepAlive(bot);
-	}
+    /**
+     * Destination of the browser.
+     *
+     * @param bot WebDriver instance
+     */
+    private static void navigateToSpam(WebDriver bot) {
+        //TODO Put your spam here
+        bot.get("https://github.com/JakeJMattson");
+    }
 
-	/**
-	 * Destination of the browser.
-	 *
-	 * @param bot
-	 * 		WebDriver instance
-	 */
-	private static void navigateToSpam(WebDriver bot)
-	{
-		//TODO Put your spam here
-		bot.get("https://github.com/JakeJMattson");
-	}
+    /**
+     * Prevent the Thread from completing and closing the browser.
+     *
+     * @param bot WebDriver instance
+     */
+    private static void keepAlive(WebDriver bot) {
+        while (!Thread.interrupted())
+            try {
+                bot.getCurrentUrl();
+            } catch (WebDriverException e) {
+                Thread.currentThread().interrupt();
+            }
 
-	/**
-	 * Dynamically place the browser on the screen.
-	 *
-	 * @param bot
-	 * 		WebDriver instance
-	 */
-	private void placeBrowser(WebDriver bot)
-	{
-		int numOfRows = (int) Math.sqrt(totalBrowsers);
-		int browsersPerRow = (int) Math.ceil((double) totalBrowsers / numOfRows);
-		int lastRow = totalBrowsers % browsersPerRow;
+        bot.quit();
+    }
 
-		if (lastRow == 0)
-			lastRow = browsersPerRow;
+    @Override
+    public void run() {
+        WebDriver bot = new ChromeDriver();
+        placeBrowser(bot);
+        navigateToSpam(bot);
+        keepAlive(bot);
+    }
 
-		Rectangle screenSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
-		double browserHeight = Math.round(screenSize.getHeight() / numOfRows);
-		double browserWidth = screenSize.getWidth();
+    /**
+     * Dynamically place the browser on the screen.
+     *
+     * @param bot WebDriver instance
+     */
+    private void placeBrowser(WebDriver bot) {
+        int numOfRows = (int) Math.sqrt(totalBrowsers);
+        int browsersPerRow = (int) Math.ceil((double) totalBrowsers / numOfRows);
+        int lastRow = totalBrowsers % browsersPerRow;
 
-		int divisor = browserIndex + 1 > totalBrowsers - lastRow ? lastRow : browsersPerRow;
-		browserWidth = Math.round(browserWidth / divisor);
+        if (lastRow == 0) {
+            lastRow = browsersPerRow;
+        }
 
-		bot.manage().window().setSize(new Dimension((int) browserWidth, (int) browserHeight));
+        Rectangle screenSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+        double browserHeight = Math.round(screenSize.getHeight() / numOfRows);
+        double browserWidth = screenSize.getWidth();
 
-		int widthSlot = (int) (browserWidth * (browserIndex % browsersPerRow));
-		int heightSlot = (int) (browserHeight * (browserIndex / browsersPerRow));
+        int divisor = browserIndex + 1 > totalBrowsers - lastRow ? lastRow : browsersPerRow;
+        browserWidth = Math.round(browserWidth / divisor);
 
-		Point browserPosition = new Point(widthSlot, heightSlot);
-		bot.manage().window().setPosition(browserPosition);
-	}
+        bot.manage().window().setSize(new Dimension((int) browserWidth, (int) browserHeight));
 
-	/**
-	 * Prevent the Thread from completing and closing the browser.
-	 *
-	 * @param bot
-	 * 		WebDriver instance
-	 */
-	private static void keepAlive(WebDriver bot)
-	{
-		while (!Thread.interrupted())
-			try
-			{
-				bot.getCurrentUrl();
-			}
-			catch (WebDriverException e)
-			{
-				Thread.currentThread().interrupt();
-			}
+        int widthSlot = (int) (browserWidth * (browserIndex % browsersPerRow));
+        int heightSlot = (int) (browserHeight * (browserIndex / browsersPerRow));
 
-		bot.quit();
-	}
+        Point browserPosition = new Point(widthSlot, heightSlot);
+        bot.manage().window().setPosition(browserPosition);
+    }
 }
--- src/main/java/me/jakejmattson/spambot/SpamController.java
+++ src/main/java/me/jakejmattson/spambot/SpamController.java
@@ -4,52 +4,44 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * Creates a group of browsers.
- *
- * @author JakeJMattson
  */
-public class SpamController
-{
-	public static void main(String[] args)
-	{
-		//Allow use of WebDrivers
-		WebDriverManager.chromedriver().setup();
+public class SpamController {
+    public static void main(String[] args) {
+        //Allow use of WebDrivers
+        WebDriverManager.chromedriver().setup();
 
-		//Arbitrary limit
-		final int MAX_THREADS = 12;
+        //Arbitrary limit
+        final int MAX_THREADS = 12;
 
-		Thread[] threads = createThreads(1);
+        Thread[] threads = createThreads(1);
 
-		while (threads.length < MAX_THREADS)
-			for (Thread currentThread : threads)
-				if (!currentThread.isAlive())
-				{
-					//If any browsers in the set were closed, kill the others
-					for (Thread deadThread : threads)
-						deadThread.interrupt();
+        while (threads.length < MAX_THREADS)
+            for (Thread currentThread : threads)
+                if (!currentThread.isAlive()) {
+                    //If any browsers in the set were closed, kill the others
+                    for (Thread deadThread : threads)
+                        deadThread.interrupt();
 
-					threads = createThreads(threads.length + 1);
-					break;
-				}
-	}
+                    threads = createThreads(threads.length + 1);
+                    break;
+                }
+    }
 
-	/**
-	 * Create and start an array of threads for monitoring.
-	 *
-	 * @param threadCount
-	 * 		Number of threads to create
-	 *
-	 * @return Threads
-	 */
-	private static Thread[] createThreads(int threadCount)
-	{
-		Thread[] threads = new Thread[threadCount];
+    /**
+     * Create and start an array of threads for monitoring.
+     *
+     * @param threadCount Number of threads to create
+     *
+     * @return Newly created threads
+     */
+    private static Thread[] createThreads(int threadCount) {
+        Thread[] threads = new Thread[threadCount];
 
-		for (int i = 0; i < threadCount; i++)
-		{
-			threads[i] = new Thread(new Browser(threads.length, i));
-			threads[i].start();
-		}
+        for (int i = 0; i < threadCount; i++) {
+            threads[i] = new Thread(new Browser(threads.length, i));
+            threads[i].start();
+        }
 
-		return threads;
-	}
+        return threads;
+    }
 }
